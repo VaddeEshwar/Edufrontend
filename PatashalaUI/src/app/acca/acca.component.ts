5ae1d9f618f40efa6bb2e3f70b3226c59108682e
VaddeEshwar <142666650+VaddeEshwar@users.noreply.github.com>
2024-11-20 18:00:05 +0530

    home pages changes and courses component adding

--- PatashalaUI/src/app/acca/acca.component.ts
+++ PatashalaUI/src/app/acca/acca.component.ts
@@ -9,46 +9,43 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./acca.component.css']
 })
 export class AccaComponent {
-  listMenuResponse:any = [];
-  caresponses:any =[];
-  responses_Subheading:any=[];
+  listMenuResponse: any = [];
+  caresponses: any = [];
+  responses_Subheading: any = [];
   menuId: any;
   constructor(private httpClient: HttpClient,
     private route: ActivatedRoute,
     private apiService: ApiService,
-    ) {
+  ) {
   }
 
-  ngOnInit(){
-    this.route.queryParams.subscribe(params=>{
-      
-      this.menuId= params['menuId'];
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+
+      this.menuId = params['menuId'];
     });
 
     // this.httpClient.get<any>("assets/data.json").subscribe((data)=>{
-      this.apiService.getData().subscribe((data:any)=>{
-      
-      this.listMenuResponse = data.listMenuSubMenu;
-          this.listMenuResponse.forEach((element: { listMenuResponse: any; }) => {
-            element.listMenuResponse.forEach((x: { responses: any; menu_Id :any;responses_Subheading:any })=>
-              {
-                // menu_Id:9 submenu_id:2
-               // if(x.menu_Id == this.menuId){
-                if(x.menu_Id == 26){
+    this.apiService.getData().subscribe((data: any) => {
 
-                  
-                  this.caresponses.push({menu:x.responses});
-                  if (x.responses_Subheading!=null)
-                      this.responses_Subheading = x.responses_Subheading;
-                }
-                
-              })
-          });
+      this.listMenuResponse = data.listMenuSubMenu;
+      this.listMenuResponse.forEach((element: { listMenuResponse: any; }) => {
+        element.listMenuResponse.forEach((x: { responses: any; menu_Id: any; responses_Subheading: any }) => {
+          // menu_Id:9 submenu_id:2
+          // if(x.menu_Id == this.menuId){
+          if (x.menu_Id == 26) {
+
+
+            this.caresponses.push({ menu: x.responses });
+            if (x.responses_Subheading != null)
+              this.responses_Subheading = x.responses_Subheading;
+          }
+
+        })
+      });
     }
-    
+
     );
-   
-   
   }
 }
 
